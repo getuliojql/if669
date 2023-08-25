604306c8eff34ec93b43a1ed6dcc1b6105e4c91b
Getúlio Junqueira <136401175+getuliojql@users.noreply.github.com>
2023-08-25 11:29:06 -0300

    add

--- lista06 - ex001.py
+++ lista06 - ex001.py
@@ -1,75 +1,56 @@
+# definindo função que cria um dicionário para o famoso informado
+def registrar(famoso):
+    registro = {'nome': famoso[:famoso.index(' - ')]}
+
+    famoso = famoso[famoso.index(' - ') + 3:].split(' ')
+
+    registro['profissao'] = famoso[0]
+    registro['avaliacao'] = famoso[1]
+    registro['mes'] = int(famoso[2])
+
+    return registro
 
 
+# definindo função que verifica se o famoso informado já havia sido informado e atualiza suas informações
+def verificar_repeticao(lista, novo_dicionario):
+    for idx_dicionario, dicionario in enumerate(lista):
+        if dicionario['nome'] == novo_dicionario['nome']:
+            lista[idx_dicionario] = novo_dicionario
+
+            return True
+
+    return False
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''# função que cria o dicionário de dado famoso
-def registrar_famoso():
-    famoso = {}
-    informacoes = str(input())
-
-    famoso['nome'] = informacoes[0:informacoes.index('-')].rstrip()
-
-    informacoes = informacoes[informacoes.index('-') + 2:].split(' ')
-
-    famoso['profissao'] = informacoes[0]
-    famoso['avaliacao'] = informacoes[1]
-    famoso['mes'] = int(informacoes[2])
-
-    return famoso
-
-
-# definindo a lista que irá conter todos os famosos e recebendo quantos serão
+# criando lista que conterá os dicionários dos famosos
 registro_geral = []
-qtd_famosos = int(input())
 
-# somando os dicionários de cada um à lista geral
-for f in range(qtd_famosos):
-    registro_geral.append(registrar_famoso())
+# recebendo os dados do famoso
+for personalidade in range(int(input())):
+    novo_registro = registrar(str(input()))
 
-# definindo listas para ajudar na hora de imprimir as informações dos fake nattys
+    # verificando se já existia; se já, atualizando suas informações, se não, adicionando-o à lista
+    if not verificar_repeticao(registro_geral, novo_registro):
+        registro_geral.append(novo_registro)
+
+# criando listas para auxiliar na impressão dos "fake nattys" e recebendo mês desejado
 fake_nattys = []
 fake_nattys_profissoes = []
 consulta_mes = int(input())
 
-# indo de dicionário em dicionário, checando se o famoso cumpre os requisitos de "fake natty do mês" e registrando-o
-for registro in registro_geral:
-    if registro['mes'] == consulta_mes and registro['avaliacao'] == 'fake':
-        fake_nattys.append(registro['nome'])
-        fake_nattys_profissoes.append(registro['profissao'])
+# registrando nome e profissão dos "fake nattys"
+for individuo in registro_geral:
+    if individuo['mes'] == consulta_mes and individuo['avaliacao'] == 'fake':
+        fake_nattys.append(individuo['nome'])
+        fake_nattys_profissoes.append(individuo['profissao'])
 
-# caso existam fake nattys a serem expostos
+# caso existam, imprimindo uma lista dos "fake nattys"
 if len(fake_nattys) > 0:
     print('Os fake nattys do mês são:')
 
-    # organizando-os por ordem alfabética
     for nome in sorted(fake_nattys):
         print(f'{nome} - {fake_nattys_profissoes[fake_nattys.index(nome)]}')
 
-# caso não existam
+# caso não existam, imprimindo mensagem de inexistência
 else:
     print('Até agora não temos ninguém pra expor na internet neste mês :(')
-# CRIAR UMA LISTA AO INVES DE UM DICIONARIO
-'''
