525dd6dacf60caeee7977f2290794520a19deba7
Getúlio Junqueira <136401175+getuliojql@users.noreply.github.com>
2024-05-06 17:07:05 -0300

    add

--- testes_monitoria.py
+++ testes_monitoria.py
@@ -1,19 +1,37 @@
-#inputs
-vm = (int(input()))
-tempo = (str(input()))
+# inputs
 
-#resolução
-if ((250 <= vm <= 260) and (tempo == 'ensolarado' or tempo == 'chuvoso')):
-    print ('Nesse dia, Senna corria em Mônaco, onde esteve no pódio 8 vezes!')
+nome = input()
+quantidade_de_musicas = int(input())
+numero_maior_de_streams = 0
+numero_menor_de_streams = 0
 
-elif ((261 <= vm <= 285) and (tempo == 'ensolarado' or tempo == 'chuvoso')):
-    print ('Senna corria em Ímola, onde infelizmente fez sua última corrida.')
+# comandos
+if quantidade_de_musicas == 0:
+    print(f'{nome} é team Taylor e não ouviu Kanye West')
 
-elif ((286 == vm <= 310) and (tempo == 'ensolarado' or tempo == 'chuvoso')):
-    print ('Claramente Spa-Francorchamps, circuito no qual Senna venceu histórico duelo com Prost depois de três largadas!')
+elif quantidade_de_musicas == 1:
+    musica = input()
+    qs = int(input())
 
-elif ((vm <= 260) and (tempo == 'neblina')):
-    print ('Senna corria em Ímola, onde infelizmente fez sua última corrida.')
+    print(f'A única música que {nome} ouviu foi {musica} com {qs} streams')
 
-elif ((vm <= 285) and (tempo == 'neblina')):
-    print ('Claramente Spa-Francorchamps, circuito no qual Senna venceu histórico duelo com Prost depois de três largadas!')
+elif quantidade_de_musicas != 0 and not quantidade_de_musicas == 1:
+    for qm in range(quantidade_de_musicas, 0, -1):
+        musica = input()
+        quantidade_de_streams = int(input())
+
+        if quantidade_de_streams > numero_maior_de_streams:
+            numero_maior_de_streams = quantidade_de_streams
+            musica_mais_escutada = musica
+
+        elif quantidade_de_streams < numero_menor_de_streams or numero_menor_de_streams == 0:
+      # if quantidade_de_streams < numero_menor_de_streams or numero_menor_de_streams == 0:   [SOLUÇÃO]
+            numero_menor_de_streams = quantidade_de_streams
+            musica_menos_escutada = musica
+
+    if numero_maior_de_streams > numero_menor_de_streams:
+        print(
+            f'A música que {nome} mais ouviu foi {musica_mais_escutada} com {numero_maior_de_streams} streams\nA música que {nome} menos ouviu foi {musica_menos_escutada} com {numero_menor_de_streams} streams')
+    elif numero_menor_de_streams == quantidade_de_streams:
+
+        print(f'A música que {nome} mais ouviu foi {musica_mais_escutada} com {numero_maior_de_streams} streams')
