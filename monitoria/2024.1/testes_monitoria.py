83d4ebb22ad5acd001f0d12640fd4d65745b1bda
Getúlio Junqueira <136401175+getuliojql@users.noreply.github.com>
2024-11-12 21:16:41 -0300

    organizing

---
+++ monitoria/2024.1/testes_monitoria.py
@@ -0,0 +1,15 @@
+while True:
+    nome = str(input())
+    idade = 2024 - int(input())
+    soma = int(input())
+    print(nome, idade + soma)
+
+    nome = str(input())
+    idade = (2024 - int(input()))
+    soma = int(input())
+    print(nome, idade + soma)
+
+    nome = str(input())
+    idade = (2024 - int(input())) // 3
+    soma = int(input())
+    print(nome, idade + soma)
