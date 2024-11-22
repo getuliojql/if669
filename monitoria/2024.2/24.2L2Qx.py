0e84614f28aafa11ee814f4964cf66f5e84043fb
Getúlio Junqueira <136401175+getuliojql@users.noreply.github.com>
2024-11-22 09:44:20 -0300

    create question

---
+++ monitoria/2024.2/24.2L2Qx.py
@@ -0,0 +1,29 @@
+contador = musica = tentativas = 0
+print('PLAYLIST DO PAPAI NOEL')
+
+while musica != 'FIM' and tentativas < 3:
+    musica = input()
+
+    if musica != 'FIM':
+        entrada = musica.replace(' ', '')
+
+        if len(entrada) >= 15 and entrada.isalpha():
+            contador += 1
+            ultima_musica = musica
+            print(f'"{musica}" foi adicionada à playlist!')
+
+        elif entrada.isnumeric():
+            contador = 0
+            tentativas += 1
+
+            if tentativas < 3:
+                print('ABORTAR! OS VILÕES OBTIVERAM ACESSO, TEREMOS QUE RECOMEÇAR.\nPLAYLIST DO PAPAI NOEL')
+
+        else:
+            print(f'"{musica}" não pôde ser adicionada à playlist! A última música adicionada foi "{ultima_musica}".')
+
+if tentativas == 3:
+    print('NAAÃO! Já está na hora do Papai Noel sair e não conseguimos construir sua playlist perfeita.')
+
+else:
+    print(f'Ufa! Conseguimos criar a playlist perfeita, tomara que o Papai Noel goste das {contador} músicas.')
