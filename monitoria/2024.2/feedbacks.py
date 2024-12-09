5e79912c1a92bc01fe85652dfd094be591b05f28
Getúlio Junqueira <136401175+getuliojql@users.noreply.github.com>
2024-12-09 17:55:09 -0300

    creat feedback log

---
+++ monitoria/2024.2/feedbacks.py
@@ -0,0 +1,58 @@
+'''
+Alunos: Felipe Costa Coutinho            [1]
+        Felipe da Silva Leite            [2]
+        Guilherme Ferreira Maximo        [3]
+        Gustavo Bizerra da Silva Izidio  [4]
+        Gustavo Ferreira Leite de Barros [5]
+⛔✅
+* nas listas de numeração par, escrever [1, 2] e revisar [3, 4, 5]
+* nas listas de numeração ímpar, escrever [3, 4, 5] e revisar [1, 2]
+''''''
+FEEDBACK 1:
+[1] - ...
+[2] - ...
+
+[3] - em andamento
+[4] - em andamento
+[5] - em andamento
+''''''
+FEEDBACK 2:
+[1] -
+[2] -
+
+[3] - 
+[4] - 
+[5] - 
+''''''
+FEEDBACK 3:
+[1] -
+[2] -
+
+[3] - 
+[4] - 
+[5] - 
+''''''
+FEEDBACK 4:
+[1] -
+[2] -
+
+[3] - 
+[4] - 
+[5] - 
+''''''
+FEEDBACK 5:
+[1] -
+[2] -
+
+[3] - 
+[4] - 
+[5] - 
+''''''
+FEEDBACK 6:
+[1] -
+[2] -
+
+[3] - 
+[4] - 
+[5] - 
+'''
