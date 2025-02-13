538c0c19cda9686ed395d88ccfb9c2a5ee510896
Getúlio Junqueira <136401175+getuliojql@users.noreply.github.com>
2025-02-13 11:21:27 -0300

    changes

--- monitoria/2024.2/testes.py
+++ monitoria/2024.2/testes.py
@@ -1,53 +1,54 @@
-receber_entradas = True
-vilões = []
+personagens_da_serie = ["Jinx", "Vi", "Heimerdinger", "Ekko", "Caitlyn", "Jayce", "Viktor", "Silco", "Claggor",
+                        "Vander", "Mylo"]
+personagens_chutados = [""] * 11
+chutes_final = []
 
-while receber_entradas:
-    entrada = input()
 
-    if entrada == 'Novo vilão - Muito Perigoso':
-        vilão = input()
-        vilões = [vilão] + vilões
-    elif entrada == 'Novo vilão - Meio perigoso':
-        vilão = input()
-        vilões = vilões + [vilão]
-    elif entrada == 'O que ele está fazendo aqui?':
-        vilão_a_remover = input()
-        for vilão in vilões:
-            if vilão == vilão_a_remover:
-                vilões.remove(vilão)
-    elif entrada == 'Vilão mais perigoso do que pensávamos':
-        indice_atual = int(input())
-        indice_novo = int(input())
-        vilão_antigo = str()
-        vilão_antigo = vilões[indice_novo]
-        vilões[indice_novo] = vilões[indice_atual]
-        vilões[indice_atual] = vilão_antigo
-    elif entrada == 'Que estranho, esses dois vilões… troque-os de lugar':
-        vilão_1 = input()
-        vilão_2 = input()
-        index_vilão_1 = vilões.index(vilão_1)
-        index_vilão_2 = vilões.index(vilão_2)
-        vilões[index_vilão_1] = vilão_2
-        vilões[index_vilão_2] = vilão_1
-    elif entrada == 'Essa posição não está de acordo, ele nem odeia carecas':
-        vilão_a_mover = input()
-        for vilão in vilões:
-            if vilão == vilão_a_mover:
-                vilões.remove(vilão)
-        vilões = vilões + [vilão_a_mover]
-    elif entrada == 'Como a lista está ficando?':
-        for vilão in vilões:
-            if vilão == vilões[-1]:
-                print(vilão, end='')
-            else:
-                print(vilão + ', ', end='')
-    elif entrada == 'Já temos nossa lista de vilões':
-        receber_entradas = False
+def adicionar(tentativa):
+    for i in range(len(personagens_da_serie)):
+        if personagens_chutados[i] == "":
+            personagens_chutados[i] = tentativa
+            print(f'Personagem {tentativa} adicionado')
+            break
 
-print('O resultado final ficou assim:')
 
-for vilão in vilões:
-    if vilão == vilões[-1]:
-        print(vilão, end='')
-    else:
-        print(vilão + ', ', end='')
+def remover(tentativa):
+    if tentativa in personagens_chutados:
+        for i in range(11):
+            if tentativa == personagens_chutados[i]:
+                personagens_chutados[i] = ""
+                break
+
+
+n_max = int(input())
+
+while n_max > 0:
+    chute = input()
+    if chute == "Adiciona":
+        tentativa = input()
+        adicionar(tentativa)
+        n_max -= 1
+    elif chute == "Remove":
+        tentativa = input()
+        remover(tentativa)
+        n_max += 1
+
+for j in personagens_chutados:
+    if j != "":
+        chutes_final += [j]
+
+print("Lista final de personagens:")
+for i in chutes_final:
+    print(i)
+
+contador = 0
+for i in range(len(chutes_final)):
+    if chutes_final[i] in personagens_da_serie:
+        contador += 1
+
+if contador == len(chutes_final):
+    print("Parabéns!! Você conseguiu acertar todos os nomes.")
+    print("UAUUU! Acho que estamos preparados para ver a segunda temporada.")
+else:
+    print("Infelizmente você perdeu...")
+    print("Eita... Vamos precisar assistir novamente.")
